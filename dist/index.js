179bc1a571c866a380e739144b71850c900998e6
Chuck Meyer <chuck.meyer@algolia.com>
2022-03-31 16:40:53 -0400

    Move from vercel to netlify

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.listen(3000, function () { return console.log('Listening'); });
-//# sourceMappingURL=index.js.map
