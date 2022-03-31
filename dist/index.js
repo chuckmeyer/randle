c9155966449ef58d1673b5a7ddccaed219cc62ac
Chuck Meyer <chuck.meyer@algolia.com>
2022-03-31 16:34:30 -0400

    Swapped to react

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.listen(3000, function () { return console.log('Listening'); });
+//# sourceMappingURL=index.js.map
