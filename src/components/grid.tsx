26ca85cbc72c40d861b38e5c9370fc998f71af40
Chuck Meyer <chuck.meyer@algolia.com>
2022-04-01 07:56:36 -0400

    bug: need triple equals

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -7,7 +7,7 @@ function Grid (props:GridProps) {
   var grid = [];
   for(var i = 0; i < props.length; i++) {
   	var line = '';
-		if ( i < props.length - 1 || props.length == 1) {
+		if ( i < props.length - 1 || props.length === 1) {
 			for(var j = 0; j < props.width; j++) {
 				const coin = Math.floor(Math.random() * 3);
 				switch(coin) {
